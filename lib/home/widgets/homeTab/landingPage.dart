a633d383488f210a72aadee948a25ebd7551b5f0
MarvinTheCoder <marvinmusambi39@gmail.com>
2020-04-23 15:06:30 +0300

    Refactored home

--- lib/home/widgets/homeTab/landingPage.dart
+++ lib/home/widgets/homeTab/landingPage.dart
@@ -1,80 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../home.dart';
-import '../reUsables/reusableButton.dart';
+import '../../widgets/reUsables/reusableButton.dart';
+import '../../widgets/tipsTab/coronaInfoGridAboveTipsTab.dart';
+import '../../widgets/tipsTab/healthUnitsNewsRow.dart';
+import '../../widgets/tipsTab/topRowCasesCard.dart';
 
-class HomeLanding extends StatefulWidget {
-  @override
-  _HomeLandingState createState() => _HomeLandingState();
-}
-
-class _HomeLandingState extends State<HomeLanding> {
-  //clicking Doctors
-  void _onTapDoctors() {
-    print('Doctors');
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return Home(showWhichTabs: 0); //
-    }));
-  }
-
-  //clicking Treatment Centers
-  void _onTapTreatCenters() {
-    print('Treatment Centers');
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return Home(showWhichTabs: 0);
-    }));
-  }
-
-  //clicking Quarantine Facilities
-  void _onTapQuarantineFac() {
-    print('Quarantine Facilities');
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return Home(showWhichTabs: 0);
-    }));
-  }
-
+class HomeLanding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 8.0),
-      child: Column(
-        children: <Widget>[
-          //above buttons on landing page
-          Container(
-            color: Colors.white70,
-            margin: EdgeInsets.all(3),
-            height: 200,
-            width: double.infinity,
-            child: Text(''),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ReUsableButton(
-                  title: 'Doctors Updates',
-                  alignment: Alignment.center,
-                  position: 0,
-                  navigateFunc: _onTapDoctors,
-                ),
-                ReUsableButton(
-                  title: 'Treatment Centers',
-                  alignment: Alignment.centerRight,
-                  position: 1,
-                  navigateFunc: _onTapTreatCenters,
-                ),
-                ReUsableButton(
-                  title: 'Quarantine Facilities',
-                  alignment: Alignment.centerLeft,
-                  position: 2,
-                  navigateFunc: _onTapQuarantineFac,
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+    return ListView(
+      children: <Widget>[
+        //cases,recovered,deaths
+        TopRowCasesCard(),
+        //more updates
+        ReUsableButton(
+          title: 'More Updates',
+          position: 0,
+          alignment: Alignment.center,
+          navigateFunc: () {},
+        ),
+        //health units and news
+        HealthUnitsNewsRow(),
+
+        Center(
+          child: Text('Feeling bad!'),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        ReUsableButton(
+          title: 'TALK TO A DOCTOR',
+          position: 0,
+          alignment: Alignment.center,
+          navigateFunc: () {},
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        CoronaInfoGridAboveTipsTab(),
+      ],
     );
   }
 }
--- lib/home/widgets/tipsTab/tips.dart
+++ lib/home/widgets/tipsTab/tips.dart
@@ -1,42 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/reUsables/reusableButton.dart';
 import 'coronaInfoGridAboveTipsTab.dart';
-import 'healthUnitsNewsRow.dart';
-import 'topRowCasesCard.dart';
 
 class Tips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        //cases,recovered,deaths
-        TopRowCasesCard(),
-        //more updates
-        ReUsableButton(
-          title: 'More Updates',
-          position: 0,
-          alignment: Alignment.center,
-          navigateFunc: () {},
-        ),
-        //health units and news
-        HealthUnitsNewsRow(),
-
-        Center(
-          child: Text('Feeling bad!'),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        ReUsableButton(
-          title: 'TALK TO A DOCTOR',
-          position: 0,
-          alignment: Alignment.center,
-          navigateFunc: () {},
-        ),
-        SizedBox(
-          height: 15,
-        ),
         CoronaInfoGridAboveTipsTab(),
       ],
     );
