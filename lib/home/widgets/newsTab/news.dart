c17c9ec53b44ff01f8e90a127ede988df39f1524
EngBett <bettthetimetraveller@gmail.com>
2020-04-23 22:46:41 +0300

    First commit

--- lib/home/widgets/newsTab/news.dart
+++ lib/home/widgets/newsTab/news.dart
@@ -1,10 +1,197 @@
+import 'package:covidappkenya/app_theme.dart';
+import 'package:covidappkenya/ui_view/twitter_list_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class News extends StatelessWidget {
+class News extends StatefulWidget {
+  const News({Key key}) : super(key: key);
+
+  @override
+  _NewsState createState() => _NewsState();
+
+}
+
+class _NewsState extends State<News> with TickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> topBarAnimation;
+
+  List<Widget> listViews = <Widget>[];
+  final ScrollController scrollController = ScrollController();
+  double topBarOpacity = 0.0;
+
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
+
+    topBarAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: animationController,
+            curve: Interval(0, 0.5, curve: Curves.fastOutSlowIn)));
+    addAllListData();
+
+    scrollController.addListener(() {
+      if (scrollController.offset >= 24) {
+        if (topBarOpacity != 1.0) {
+          setState(() {
+            topBarOpacity = 1.0;
+          });
+        }
+      } else if (scrollController.offset <= 24 &&
+          scrollController.offset >= 0) {
+        if (topBarOpacity != scrollController.offset / 24) {
+          setState(() {
+            topBarOpacity = scrollController.offset / 24;
+          });
+        }
+      } else if (scrollController.offset <= 0) {
+        if (topBarOpacity != 0.0) {
+          setState(() {
+            topBarOpacity = 0.0;
+          });
+        }
+      }
+    });
+
+
+    super.initState();
+  }
+
+
+  Future<bool> getData() async {
+    await Future<dynamic>.delayed(const Duration(milliseconds: 100));
+    return true;
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('News')),
+    return Scaffold(
+      backgroundColor: AppTheme.white,
+      body: FutureBuilder<bool>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (!snapshot.hasData) {
+            return const SizedBox();
+          } else {
+            return Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+
+                  //Appbar
+                  appBar(),
+
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        getMainListViewUI(),
+                      ],
+                    ),//Stack
+                  ),//Container
+
+                  SizedBox(
+                    height: MediaQuery.of(context).padding.bottom,
+                  )
+
+                ],
+              ),
+            );
+          }
+        },
+      ),
     );
   }
+
+  Widget getMainListViewUI() {
+    return FutureBuilder<bool>(
+      future: getData(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (!snapshot.hasData) {
+          return const SizedBox();
+        } else {
+          return ListView.builder(
+            controller: scrollController,
+            itemCount: listViews.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (BuildContext context, int index) {
+              animationController.forward();
+              return listViews[index];
+            },
+          );
+        }
+      },
+    );
+  }
+
+  void addAllListData() {
+    const int count = 8;
+
+    listViews.add(
+      TwitterListView(
+        mainScreenAnimation: Tween<double>(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation(
+                parent: animationController,
+                curve: Interval((1 / count) * 5, 1.0,
+                    curve: Curves.fastOutSlowIn))),
+        mainScreenAnimationController: animationController,
+      ),
+    );
+
+
+  }
+
+  Widget appBar() {
+    return SizedBox(
+      height: AppBar().preferredSize.height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 8, left: 20),
+            child: Container(
+              width: AppBar().preferredSize.height - 8,
+              height: AppBar().preferredSize.height - 8,
+            ),
+          ),
+          Expanded(
+              child: Center(
+                child: Row(
+                  children: <Widget>[
+
+                    Text(
+                      'News',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: AppTheme.nearlyDarkBlue,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ), //Text
+
+                    Container(
+                      width: 10.0,
+                      height: 50.0,
+                    ), //Container
+
+
+                  ],
+                ), //Row
+              ) //Center
+
+          ),
+        ],
+      ),
+    );
+  }
+
+
 }
